78cdfb8e0afa4d5abd88567715b4e2419dd64699
Hatim-BE <[nothatim@gmail.com])>
2024-12-25 22:42:06 +0100

    utils

---
+++ utils/db.py
@@ -0,0 +1,15 @@
+import mysql.connector
+import os
+
+def get_database_connection():
+    try:
+        conn = mysql.connector.connect(
+            host=os.getenv('DB_HOST'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+            database=os.getenv('DB_NAME')
+        )
+        return conn
+    except mysql.connector.Error as err:
+        print(f"Error connecting to database: {err}")
+        return None
